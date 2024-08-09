7c03e54640722b78445b889aebcea4ba2e69ed95
Nicholas Chandler <chandln@wwu.edu>
2024-08-08 20:22:23 -0700

    Fixed the js with Elias

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,30 +1,53 @@
-// scroll to top functionality
-const scrollUp = document.querySelector("#scroll-up");
+"use strict";
 
-scrollUp.addEventListener("click", () => {
-  window.scrollTo({
-    top: 0,
-    left: 0,
-    behavior: "smooth",
-  });
-});
+(function() {
 
 
-// Nav hamburgerburger selections
-const burger = document.querySelector("#burger-menu");
-const ul = document.querySelector("nav ul");
-const nav = document.querySelector("nav");
+  /**
+  * Add a function that will be called when the window is loaded.
+  */
+  window.addEventListener("load", init);
 
-burger.addEventListener("click", () => {
-  ul.classList.toggle("show");
-});
+  /**
+  * CHANGE: Describe what your init function does here.
+  */
+  function init() {
+    // THIS IS THE CODE THAT WILL BE EXECUTED ONCE THE WEBPAGE LOADS
+    // scroll to top functionality
+    const scrollUp = document.querySelector("#scroll-up");
 
-// Close hamburger menu when a link is clicked
-// Select nav links
-const navLink = document.querySelectorAll(".nav-link");
+    scrollUp.addEventListener("click", () => {
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+      });
+    });
 
-navLink.forEach((link) =>
-  link.addEventListener("click", () => {
-    ul.classList.remove("show");
-  })
-);
+
+    // Nav hamburgerburger selections
+    const burger = document.querySelector("#burger-menu");
+    const ul = document.querySelector("nav ul");
+    const nav = document.querySelector("nav");
+
+    burger.addEventListener("click", () => {
+      ul.classList.toggle("show");
+    });
+
+    // Close hamburger menu when a link is clicked
+    // Select nav links
+    const navLink = document.querySelectorAll(".nav-link");
+
+    navLink.forEach((link) =>
+      link.addEventListener("click", () => {
+        ul.classList.remove("show");
+      })
+  );
+
+
+  }
+  
+  
+
+  
+})();
